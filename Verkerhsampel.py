40c5382c1b281afa77a26b7723cfb5f91cc9d278
Neos <neos1400@gmail.com>
2016-03-01 21:14:34 +0100

    Sehr schön =D

--- Verkerhsampel.py
+++ Verkerhsampel.py
@@ -10,7 +10,7 @@ print ("STRG+C beendet das Programm")
 try:
     while True:
         time.sleep(2)
-        GPIO.output(Ampel[gruen],False); GPIO.output(Ampel[gelb,True])
+        GPIO.output(Ampel[gruen],False); GPIO.output(Ampel[gelb],True)
         time.sleep(0.6)
         GPIO.output(Ampel[gelb],False); GPIO.output(Ampel[rot], True)
         time.sleep(2)
