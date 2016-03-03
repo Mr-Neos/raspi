8cceff7079f5ccd349a4d4e274fd776af03aff2e
Neos <neos1400@gmail.com>
2016-03-03 22:09:08 +0100

    Segundo proyecto con my Padre

--- Ampel02.py
+++ Ampel02.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
-rot     =0; gelb    =1; gruen   =2; blau  =3
+rot     =0; gelb    =1; gruen   =2; blau   =3
 Ampel=[4,18,23,24]
 GPIO.setup(Ampel[rot], GPIO.OUT, initial=False)
 GPIO.setup(Ampel[gelb], GPIO.OUT, initial=False)
@@ -13,6 +13,7 @@ try:
         time.sleep(2)
         GPIO.output(Ampel[gruen],False); GPIO.output(Ampel[gelb],True)
         time.sleep(0.6)
+        GPIO.output(Ampel[gelb],False); GPIO.output(Ampel[rot],True)
         for i in range(10):
             GPIO.output(Ampel[blau],True); time.sleep(0.05)
             GPIO.output(Ampel[blau],False); time.sleep(0.5)
